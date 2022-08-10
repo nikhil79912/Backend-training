19e9ede1452dd864e3abf3e326477a22327b591a
nikhil <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>
2022-08-10 12:06:59 +0530

    this is assignmrnt

--- src/routes/route.js
+++ src/routes/route.js
@@ -18,6 +18,7 @@
 const express = require('express');
 const underscore=require('underscore');
 const lodash=require('lodash');
+const { sum } = require('lodash');
 // const abc =require('./intru')
 // const log= require('../logger/logger')
 // const assign1=require('../util/helper')
@@ -60,6 +61,7 @@ router.get('/cohort-members',function(req,res){
     let students=['nik','did','sis']
     res.send(students)
 })
+router.get('')
 router.get('/student-details/:name',function(req,res){
     console.log(name)
     let details="nikhil singh"
@@ -71,6 +73,33 @@ router.get('/test-you',function(req, res) {
 })
 router.get('give-me', function(req,res){
     res.send('i luv thor')
+})
+router.get('/sol1',function (req, res) {
+    let arr= [1,2,3,5,6,7]
+    let sum=0
+    for(let i=0;i<arr.length;i++){
+        sum=sum+arr[i]
+    }
+    let c=arr.length+1
+    let count=c*(c+1)/2
+    let missingNumber=count-sum
+    // res.send(missingNumber)
+    res.send(  { data: missingNumber  })
+    console.log(missingNumber)
+})
+router.get('/sol2',function(req,res){
+   let arr=[33, 34, 35, 37, 38]
+    let sum1=0
+   for(let i=0;i<arr.length;i++){
+    sum1=sum1+arr[i]
+   }
+   let c=arr.length+1
+   let first=arr[0]
+   let last=arr[arr.length-1]
+   let cont=c*(first+last)/2
+   let missingNumber=cont-sum1
+res.send({data:missingNumber})
+console.log(missingNumber)
 })
 module.exports = router;
 // adding this comment for no reason
