9ef660eba0b1c57461a3138b084186c13395a0dd
nikhil <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>
2022-08-31 00:00:47 +0530

    try catch status code

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,46 @@
-const authenticate = function(req, req, next) {
+const jwt = require("jsonwebtoken");
+
+
+const authenticate = function (req, res, next) {
     //check the token in request header
     //validate this token
+    // let token = req.headers["x-Auth-token"]
+    {
+        try {
+            let token = req.headers["x-Auth-token"];
+            if (!token) token = req.headers["x-auth-token"];
+            if (!token) return res.send({ status: false, msg: "token must be present in the request header" })
+            let decodedToken = jwt.verify(token, 'functionup-thorium')
 
-    next()
+            if (!decodedToken) return res.status(401).send({ status: false, msg: "token is not valid" })
+            next()
+        } catch (err) {
+            console.log("This is the error :", err.message)
+            res.status(500).send({ msg: " server Error", error: err.message })
+
+        }
+    }
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
+const authorise = function (req, res, next) {
+    try {
+        let token = req.headers["x-auth-token"];
+        let decodedToken = jwt.verify(token, 'functionup-thorium')
+        let userToBeModified = req.params.userId
+        //userId for the logged-in user
+        let userLoggedIn = decodedToken.userId
+
+        //userId comparision to check if the logged-in user is requesting for their own data
+        if (userToBeModified != userLoggedIn) return res.status(403).send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
+
+        next()
+    } catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: " server Error", error: err.message })
+
+    }   // comapre the logged in user's id and the id in request
+
 }
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
